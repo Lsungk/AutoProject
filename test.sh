4e6f4c9e82e74ba32523f1141d659062dabe712d
Lsungk <126836705+Lsungk@users.noreply.github.com>
2024-08-02 14:00:24 +0900

    Update test.sh 12

--- test.sh
+++ test.sh
@@ -15,30 +15,30 @@ systemctl start sshd
 systemctl enable sshd
 
 # Openstack 계정 생성 및 openstack 계정 접속
-useradd -s /bin/bash -d /opt/openstack -m openstack
-chmod +x /opt/openstack
+useradd -s /bin/bash -d /opt/stack -m stack
+chmod +x /opt/stack
 
-echo "openstack ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/openstack
+echo "stack ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/stack
 
 # gnutls-bin 설치
 apt install -y gnutls-bin
 
 # git clone (devstack 설치)
-su - openstack -c "git clone https://opendev.org/openstack/devstack"
+su - stack -c "git clone https://opendev.org/openstack/devstack"
 
 # devstack 디렉터리 이동 및 local.conf 복사
 
-cp /opt/openstack/devstack/samples/local.conf ./local.conf"
+cp /opt/stack/devstack/samples/local.conf ./local.conf"
 
 # IP 저장
 IP=`hostname -I | cut -f 1 -d ' '`"
 
 # local.conf 파일 수정
-sudo sed -i 's/ADMIN_PASSWORD=nomoresecret/ADMIN_PASSWORD=openstack/g' /opt/openstack/devstack/local.conf
-sudo sed -i 's/DATABASE_PASSWORD=stackdb/DATABASE_PASSWORD=openstack/g' /opt/openstack/devstack/local.conf
-sudo sed -i 's/RABBIT_PASSWORD=stackqueue/RABBIT_PASSWORD=openstack/g' /opt/openstack/devstack/local.conf
-sudo sed -i 's/SERVICE_PASSWORD=$ADMIN_PASSWORD/SERVICE_PASSWORD=openstack/g' /opt/openstack/devstack/local.conf
-sudo sed -i 's/HOST_IP=w.x.y.z/HOST_IP=$IP/g' /opt/openstack/devstack/local.conf
+sudo sed -i 's/ADMIN_PASSWORD=nomoresecret/ADMIN_PASSWORD=openstack/g' /opt/stack/devstack/local.conf
+sudo sed -i 's/DATABASE_PASSWORD=stackdb/DATABASE_PASSWORD=openstack/g' /opt/stack/devstack/local.conf
+sudo sed -i 's/RABBIT_PASSWORD=stackqueue/RABBIT_PASSWORD=openstack/g' /opt/stack/devstack/local.conf
+sudo sed -i 's/SERVICE_PASSWORD=$ADMIN_PASSWORD/SERVICE_PASSWORD=openstack/g' /opt/stack/devstack/local.conf
+sudo sed -i 's/HOST_IP=w.x.y.z/HOST_IP=$IP/g' /opt/stack/devstack/local.conf
 
 # disutils 설치
 apt install -y python3.10-distutils
@@ -49,10 +49,11 @@ git config --global http.postBuffer 1048576000
 git config --global core.longpaths true
 git config --global core.compression -1
 
-su - openstack -c "sudo git clone --depth=1 https://opendev.org/openstack/neutron.git --branch stable/zed"
+su - stack -c "sudo git clone --depth=1 https://opendev.org/openstack/neutron.git --branch stable/zed"
 
 # 쉘 실행
-sh /opt/openstack/devstack/stack.sh
+sh /opt/stack/devstack/unstack.sh
+sh /opt/stack/devstack/stack.sh
 
 echo --------------------------------------------------
 echo "                    종 료                       "
