b86d793292ef962aaef390d4577aaa6174457e21
Lsungk <126836705+Lsungk@users.noreply.github.com>
2024-08-02 11:50:47 +0900

    Update test.sh 2

--- test.sh
+++ test.sh
@@ -20,41 +20,39 @@ chmod +x /opt/stack
 
 echo "stack ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/stack
 
-sudo -u openstack -i
-
 # gnutls-bin 설치
-sudo apt install -y gnutls-bin
+su - openstack -c "apt install -y gnutls-bin"
 
 # git clone (devstack 설치)
-git clone https://opendev.org/openstack/devstack
+su - openstack -c "git clone https://opendev.org/openstack/devstack"
 
 # devstack 디렉터리 이동 및 local.conf 복사
-cd /devstack
-cp ./samples/local.conf local.conf
+su - openstack -c "cd /devstack"
+su - openstack -c "cp ./samples/local.conf local.conf"
 
 # IP 저장
-IP=`hostname -I | cut -f 1 -d ' '`
+su - openstack -c "IP=`hostname -I | cut -f 1 -d ' '`"
 
 # local.conf 파일 수정
-sed -i 's/ADMIN_PASSWORD=nomoresecret/ADMIN_PASSWORD=openstack/g' local.conf
-sed -i 's/DATABASE_PASSWORD=stackdb/DATABASE_PASSWORD=openstack/g' local.conf
-sed -i 's/RABBIT_PASSWORD=stackqueue/RABBIT_PASSWORD=openstack/g' local.conf
-sed -i 's/SERVICE_PASSWORD=$ADMIN_PASSWORD/SERVICE_PASSWORD=openstack/g' local.conf
-sed -i 's/HOST_IP=w.x.y.z/HOST_IP=$IP/g' local.conf
+su - openstack -c "sed -i 's/ADMIN_PASSWORD=nomoresecret/ADMIN_PASSWORD=openstack/g' local.conf"
+su - openstack -c "sed -i 's/DATABASE_PASSWORD=stackdb/DATABASE_PASSWORD=openstack/g' local.conf"
+su - openstack -c "sed -i 's/RABBIT_PASSWORD=stackqueue/RABBIT_PASSWORD=openstack/g' local.conf"
+su - openstack -c "sed -i 's/SERVICE_PASSWORD=$ADMIN_PASSWORD/SERVICE_PASSWORD=openstack/g' local.conf"
+su - openstack -c "sed -i 's/HOST_IP=w.x.y.z/HOST_IP=$IP/g' local.conf"
 
 # disutils 설치
-sudo apt install -y python3.10-disutils
+su - openstack -c "sudo apt install -y python3.10-disutils"
 
 # git config 추가
-git config --global http.sslVerify false
-git config --global http.postBuffer 1048576000
-git config --global core.longpaths true
-git config --global core.compression -l
+su - openstack -c "git config --global http.sslVerify false"
+su - openstack -c "git config --global http.postBuffer 1048576000"
+su - openstack -c "git config --global core.longpaths true"
+su - openstack -c "git config --global core.compression -l"
 
-git clone --depth=1 https://opendev.org/openstack/neutron.gi --branch stable/zed
+su - openstack -c "git clone --depth=1 https://opendev.org/openstack/neutron.gi --branch stable/zed"
 
 # 쉘 실행
-sh /stack.sh
+su - openstack -c "sh /stack.sh"
 
 echo --------------------------------------------------
 echo                        종료
